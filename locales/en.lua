525b4194b9f2b5d08c40f9852d5f3ad08f713ee2
EpicCat <78860645+CptnCat@users.noreply.github.com>
2024-08-24 19:05:16 +0200

    Create en.lua

---
+++ locales/en.lua
@@ -0,0 +1,24 @@
+Locales['en'] = {
+    -- Menu
+    ['carkeymenu_header']  = 'Vehicle Keys',
+    ['carkey_manage_header']  = 'Your Vehicle Keys',
+    ['carkey_manage']  = 'Key Management',
+    ['steal_keys'] = 'Steal Key',
+    ['copy_key'] = 'Duplicate key',
+    ['delete_key'] = 'Delete key',
+    ['owned_field'] = '[OWNED]',
+    -- Notifications
+    ['vehicle_locked'] = 'This vehicle has been ~r~locked~s~',
+    ['vehicle_unlocked'] = 'This vehicle has been ~g~unlocked~s~',
+    ['no_keys'] = 'You do not have a key for this vehicle.',
+    ['no_vehicles_neraby'] = '~r~There are no vehicles nearby.',
+    ['already_own_key'] = '~r~You already have a key for this vehicle!',
+    ['wrong_seat'] = '~r~You must be in the driver\'s seat for this!',
+    ['no_vehicle_sit'] = '~r~You must be sitting in a vehicle.',
+    ['no_person_nearby'] = '~r~No person nearby.',
+    ['not_owned_vehicle_copy'] = '~r~You cannot give away a key that you do not own.',
+    ['received_key'] = 'You have received a ~y~key~s~ for a vehicle (~y~%s~s~).',
+    ['received_copy_key'] = 'You have received a ~y~duplicate key~s~ for a vehicle (~y~%s~s~).',
+    ['gave_copy_key'] = 'You have given a duplicate key (~y~%s~s~) to someone!',
+    ['key_removed'] = 'A ~r~key~s~ for a vehicle has been removed from you (~r~%s~s~).',
+}
