8acf820cdf2e0afd21deac01a31535a563e98dfc
EpicCat <78860645+CptnCat@users.noreply.github.com>
2024-08-24 23:56:55 +0200

    Create de.lua (thanks to Verox)

---
+++ locales/de.lua
@@ -0,0 +1,24 @@
+Locales['de'] = {
+    -- Menu
+    ['carkeymenu_header']  = 'Fahrzeugschlüssel',
+    ['carkey_manage_header']  = 'Deine Schlüssel',
+    ['carkey_manage']  = 'Schlüsselmanagement',
+    ['steal_keys'] = 'Schlüssel klauen',
+    ['copy_key'] = 'Schlüssel kopieren',
+    ['delete_key'] = 'Schlüssel löschen',
+    ['owned_field'] = '[EIGENTUM]',
+    -- Notifications
+    ['vehicle_locked'] = 'Dein Fahrzeug wurde ~r~abgeschlossen~s~',
+    ['vehicle_unlocked'] = 'Dein Fahrzeug wurde ~g~aufgeschlossen~s~',
+    ['no_keys'] = 'Du besitzt für dieses Fahrzeug keinen Schlüssel.',
+    ['no_vehicles_neraby'] = '~r~Es befindet sich kein Fahrzeug in deiner Nähe.',
+    ['already_own_key'] = '~r~Du besitzt bereits einen Schlüssel für dieses Fahrzeug!',
+    ['wrong_seat'] = '~r~Du musst auf dem Fahrersitz sitzen um dies zu tun!',
+    ['no_vehicle_sit'] = '~r~Du musst in einem Fahrzeug sitzen.',
+    ['no_person_nearby'] = '~r~Es befindet sich keine Person in deiner Nähe.',
+    ['not_owned_vehicle_copy'] = '~r~Du kannst keinen Schlüssel weitergeben den du nicht besitzt.',
+    ['received_key'] = 'Du hast einen ~y~Schlüssel~s~ für das Kennzeichen (~y~%s~s~) erhalten.',
+    ['received_copy_key'] = 'Du hast einen ~y~kopierten Schlüssel~s~ für das Kennzeichen (~y~%s~s~) erhalten.',
+    ['gave_copy_key'] = 'Du hast einen ~y~kopierten Schlüssel~s~ für das Kennzeichen (~y~%s~s~) weitergegeben!',
+    ['key_removed'] = 'Der ~r~Schlüssel~s~ mit dem Kennzeichen (~r~%s~s~) wurde gelöscht.',
+}
